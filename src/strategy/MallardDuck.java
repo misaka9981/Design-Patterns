9db8b7017610b9735146626e322b475d4dff96dd
misaka9981 <932353913@qq.com>
2021-10-13 15:53:40 +0800

    mallard duck ，继承duck基类

---
+++ src/strategy/MallardDuck.java
@@ -0,0 +1,13 @@
+package src.strategy;
+
+public class MallardDuck extends Duck{
+
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }  
+    
+    public void display() {
+        System.out.println("I'm a read Mallard duck");
+    }
+}
