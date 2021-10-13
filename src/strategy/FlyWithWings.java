9c0c5674cb90f8d44dbf9ce5575c606b6ea7b7fc
misaka9981 <932353913@qq.com>
2021-10-13 16:00:03 +0800

    实现飞行行为接口

---
+++ src/strategy/FlyWithWings.java
@@ -0,0 +1,11 @@
+package src.strategy;
+
+public class FlyWithWings implements FlyBehavior{
+
+    @Override
+    public void fly() {
+        // TODO Auto-generated method stub
+        System.out.println("I'm flying!!");
+    }
+    
+}
