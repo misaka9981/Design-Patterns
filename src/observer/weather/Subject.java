51c140e0b2381190f3c53277ec2e9ae066ad019d
misaka9981 <932353913@qq.com>
2021-10-18 20:23:25 +0800

    create interface

---
+++ src/observer/weather/Subject.java
@@ -0,0 +1,12 @@
+package src.observer.weather;
+
+
+/**
+ * Subject
+ */
+public interface Subject {
+
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
